c77b3956f56e3e6fd6e5b984dd7f452f73321031
YuliusYogiYuwono <114727124+YuliusYogiYuwono@users.noreply.github.com>
2022-10-07 21:48:13 +0700

    Project Penambahan Fitur CRUD
    Project Penambahan Fitur CRUD

--- pw/index.php
+++ pw/index.php
@@ -1,57 +1,69 @@
 <?php
-// koneksi ke database
-$conn = mysqli_connect("localhost", "root", "", "prakweb_2022_b_203040125");
-
-// ambil dari tabel buku query
-$result = mysqli_query($conn, "SELECT * FROM buku");
-
-// ubah data ke dalam array
-// $row = mysqli_fetch_row($result); // array numerik
-// $row = mysqli_fetch_assoc($result); // array associative
-// $row = mysqli_fetch_array($result); // keduanya
-$rows = [];
-while ($row = mysqli_fetch_assoc($result)) {
-  $rows[] = $row;
-}
-// tampung ke variabel buku
-$bk = $rows;
+  include('function.php'); //agar index terhubung dengan database, maka koneksi sebagai penghubung harus di include
+  
 ?>
-
 <!DOCTYPE html>
-<html lang="en">
-<link rel="stylesheet" type="" href="style.css">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Buku yang saya jual</title>
-</head>
+<html>
+  <head>
+    <title>Just Read It </title>
+      <link rel="stylesheet" type="text/css" href="style.css">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+     
+  </head>
+  <body>
+    <center><h1><b><u>Just Read It !</u></b></h1><center>
 
-<body>
+    <br/>
+    <table>
+      <thead>
+        <tr>
+          <th>No</th>
+          <th>Nama</th>
+          <th>Author</th>
+          <th>Jumlah Halaman</th>
+          <th>Harga</th>
+          <th>Gambar</th>
+          <th>Action</th>
+        </tr>
+    </thead>
+    <tbody>
+      <?php
+      // jalankan query untuk menampilkan semua data diurutkan berdasarkan nim
+      $query = "SELECT * FROM buku ORDER BY no ASC";
+      $result = mysqli_query($koneksi, $query);
+      //mengecek apakah ada error ketika menjalankan query
+      if(!$result){
+        die ("Query Error: ".mysqli_errno($koneksi).
+           " - ".mysqli_error($koneksi));
+      }
 
-  <center><h1>Daftar Buku Mahasiswa</h1></center>
+      //buat perulangan untuk element tabel dari data mahasiswa
+      $no = 1; //variabel untuk membuat nomor urut
+      // hasil query akan disimpan dalam variabel $data dalam bentuk array
+      // kemudian dicetak dengan perulangan while
+      while($row = mysqli_fetch_assoc($result))
+      {
+      ?>
+       <tr>
+          <td><?php echo $no; ?></td>
+          <td><?php echo $row['nama']; ?></td>
+           <td><?php echo $row['author']; ?></td>
+           <td><?php echo $row['jumlah_halaman']; ?></td>
+           <td>Rp.<?php echo $row['harga']; ?></td>
+          <td style="text-align: center;"><img src="gambar/<?php echo $row['gambar']; ?>" style="width: 120px;"></td>
+          <td>
+              <a class='btn btn-primary btn-sm' href="edit_produk.php?id=<?php echo $row['no']; ?>">Update Data</a>
+              <a class='btn btn-danger btn-sm' href="proses_hapus.php?id=<?php echo $row['no']; ?>" onclick="return confirm('Anda yakin akan menghapus data ini?')">Hapus Data</a>
+          </td>
+      </tr>
+         
+      <?php
+        $no++; //untuk nomor urut terus bertambah 1
+      }
+      ?>
+    </tbody>
+    </table>
+    <a class="btn btn-primary" href="tambah_produk.php" role="button">Tambahkan Produk</a>
 
-<center><table border="1" cellpading="10" cellspacing="0"></center>
-    <tr bgcolor="#FFE4C4">
-      <th>no</th>
-      <th>Nama buku</th>
-      <th>Penulis</th>
-      <th>Gambar</th>
-      <th>Harga</th>
-    </tr>
-    <?php $i = 1; ?>
-    <?php foreach ($bk as $rows) : ?>
-    <tr>
-        <td><?= $rows["id"]; ?></td>
-        <td style="vertical-align : middle;text-align:center;"><?= $rows["nama_buku"]; ?></td>
-        <td style="vertical-align : middle;text-align:center;"><?= $rows["penulis_buku"]; ?></td>
-        <td>
-          <figure class="image is-120x120">
-            <img src="img/<?= $rows["img"]; ?>" width="100" alt="">
-          </figure>
-        </td>
-        <td style="vertical-align : middle;text-align:center;"><b><?= $rows["harga"]; ?></td></b>
-        
-      <?php endforeach; ?>
-
+  </body>
 </html>
