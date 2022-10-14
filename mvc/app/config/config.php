c9e8399eecb43fd5db83db116ebf3e51e6c97781
YuliusYogiYuwono <114727124+YuliusYogiYuwono@users.noreply.github.com>
2022-10-14 14:31:54 +0700

    Delete mvc directory

--- mvc/app/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('BASEURL', 'http://localhost/prakweb_b_203040125/mvc/public');
-
-// DB
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'phpmvc');
